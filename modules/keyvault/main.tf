407c904f5cb08de268782d9c1c7ec679ed8941b9
ramkrishnapathak <ramkrishnapathak@gmail.com>
2025-07-09 09:12:22 -0700

    Update main.tf

--- modules/keyvault/main.tf
+++ modules/keyvault/main.tf
@@ -2,9 +2,7 @@ resource "azurerm_key_vault" "this" {
   name                        = var.keyvault_name
   location                    = "Canada East"
   resource_group_name         = var.resource_group_name
-  tenant_id                   = var.tenant_id
   sku_name                    = "standard"
-
   public_network_access_enabled = false
   soft_delete_retention_days    = 7
 }
