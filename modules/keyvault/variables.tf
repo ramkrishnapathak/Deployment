76892995ef5b08509b3edb3c5867391ef4f46e3a
ramkrishnapathak <ramkrishnapathak@gmail.com>
2025-07-09 09:13:37 -0700

    Update variables.tf

--- modules/keyvault/variables.tf
+++ modules/keyvault/variables.tf
@@ -7,8 +7,3 @@ variable "resource_group_name" {
   description = "The name of the resource group."
   type        = string
 }
-
-variable "tenant_id" {
-  description = "The Azure AD tenant ID."
-  type        = string
-}
